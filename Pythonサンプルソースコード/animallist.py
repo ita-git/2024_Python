0567f7105dd7f1c68c3cc207bc3ec244d790fe24
koutarouokada <koutarou.okada@zigieworks.com>
2024-05-09 21:50:13 +0900

    sample

---
+++ Pythonサンプルソースコード/animallist.py
@@ -0,0 +1,19 @@
+import sys
+args = sys.argv
+
+#引数を変数に代入
+place = int(args[1]) 
+name = args[2]
+
+#リスト anilmals の作成
+animals = ["giraffe", "tiger", "zebra", "elephant", "lion"]
+#引数の要素数の位置に、挿入
+animals.insert(place, name)
+#最後の要素を削除
+length = len(animals)
+del animals[length - 1]
+#ソート
+animals.sort()
+
+#リスト全体を出力
+print(animals, end="")
